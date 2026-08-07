9448824929474d39b1b7e8c16f65b5f850ffce52
ginafathima <ginafathimaa@gmail.com>
2026-08-07 11:45:56 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.simplecalculator;
+package com.example.lifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,103 +15,67 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText numInput1, numInput2;
-    private TextView resultText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        Button btn;
+        btn = findViewById(R.id.button);
 
-        numInput1 = findViewById(R.id.numInput1);
-        numInput2 = findViewById(R.id.numInput2);
-        resultText = findViewById(R.id.resultText);
+        btn.setOnClickListener(new View.OnClickListener() {
 
-        Button btnAdd = findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                performCalculation('+');
+            public void onClick(View view) {
 
+                btnOnclick();
             }
         });
-
-        Button btnSubtract = findViewById(R.id.btnSubtract);
-        btnSubtract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                performCalculation('-');
-
-            }
-        });
-
-        Button btnMultiply = findViewById(R.id.btnMultiply);
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                performCalculation('*');
-
-            }
-        });
-
-        Button btnDivide = findViewById(R.id.btnDivide);
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                performCalculation('/');
-
-            }
-        });
+        Log.i("my tag", "oncreate");
+        Toast.makeText(this, "oncreate", Toast.LENGTH_SHORT).show();
     }
 
-    private void performCalculation(char operation) {
+    private void btnOnclick() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
 
-        String input1 = numInput1.getText().toString();
-        String input2 = numInput2.getText().toString();
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("my tag", "onrestart");
+        Toast.makeText(this, "onresrart", Toast.LENGTH_SHORT).show();
+    }
 
-        double num1 = Double.parseDouble(input1);
-        double num2 = Double.parseDouble(input2);
-        double result = 0;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("my tag", "onstart");
+        Toast.makeText(this, "onsrart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("my tag", "onresume");
+        Toast.makeText(this, "onresume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("my tag", "onpause");
+        Toast.makeText(this, "onpause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("my tag", "onstop");
+        Toast.makeText(this, "onstop", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("my tag", "ondestroy");
+        Toast.makeText(this, "ondestroy", Toast.LENGTH_SHORT).show();
+    }
 
-        switch (operation) {
 
-            case '+':
-                result = num1 + num2;
-                break;
-
-            case '-':
-                result = num1 - num2;
-                break;
-
-            case '*':
-                result = num1 * num2;
-                break;
-
-            case '/':
-                if (num2 == 0) {
-                    resultText.setText("Cannot divide by zero");
-                    return;
-                }
-
-                result = num1 / num2;
-                break;
-
-            default:
-                resultText.setText("Invalid operation");
-                return;
         }
-        resultText.setText("Result: " + result);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
