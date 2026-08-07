9dfdec56ffa7db2c304a63cf75fe23128c0af852
ginafathima <ginafathimaa@gmail.com>
2026-08-07 11:38:12 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.simplecalculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText numInput1, numInput2;
+    private TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        numInput1 = findViewById(R.id.numInput1);
+        numInput2 = findViewById(R.id.numInput2);
+        resultText = findViewById(R.id.resultText);
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCalculation('+');
+
+            }
+        });
+
+        Button btnSubtract = findViewById(R.id.btnSubtract);
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCalculation('-');
+
+            }
+        });
+
+        Button btnMultiply = findViewById(R.id.btnMultiply);
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCalculation('*');
+
+            }
+        });
+
+        Button btnDivide = findViewById(R.id.btnDivide);
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performCalculation('/');
+
+            }
+        });
+    }
+
+    private void performCalculation(char operation) {
+
+        String input1 = numInput1.getText().toString();
+        String input2 = numInput2.getText().toString();
+
+        double num1 = Double.parseDouble(input1);
+        double num2 = Double.parseDouble(input2);
+        double result = 0;
+
+        switch (operation) {
+
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    resultText.setText("Cannot divide by zero");
+                    return;
+                }
+
+                result = num1 / num2;
+                break;
+
+            default:
+                resultText.setText("Invalid operation");
+                return;
+        }
+        resultText.setText("Result: " + result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
